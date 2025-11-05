3dbd9f39e20d333a0121606cdb27bac165f58a70
Fernando Davila <fernandodavilam@hotmail.com>
2025-11-04 23:34:25 -0500

    Update app components and server app

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+     <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
